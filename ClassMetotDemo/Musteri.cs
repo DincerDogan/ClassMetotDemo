b12e59060516985f03b21b05c65f6fc30e8f2e5f
DincerDogan <dincerdogan@gmail.com>
2021-03-02 03:24:19 +0300

    Add project files.

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        public int musteriID { get; set; }
+        public string musteriAdi { get; set; }
+        public string musteriSoyAdi { get; set; }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.musteriID = 25;
+            musteri1.musteriAdi = "Mehmet";
+            musteri1.musteriSoyAdi = "Osman";
+            
+
+            Musteri musteri2 = new Musteri();
+            musteri2.musteriID = 25;
+            musteri2.musteriAdi = "Mehmet2";
+            musteri2.musteriSoyAdi = "Osman2";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.musteriID = 25;
+            musteri3.musteriAdi = "Mehmet3";
+            musteri3.musteriSoyAdi = "Osman3";
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Listele(musteri2);
+            musteriManager.Sil(musteri3);
+            Console.WriteLine("------------------");
+
+        }
+    }
+}
